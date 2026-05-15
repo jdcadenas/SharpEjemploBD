d2602077c805bcbecc60684b96e55bca13409142
josecastro44 <castrojuego88@gmail.com>
2026-05-15 11:39:01 -0400

    Solución Laboratorio - Leal y Hernández

---
+++ ejemploBd/GestionModulos.Designer.cs
@@ -0,0 +1,78 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: usuario
+ * Fecha: 15/5/2026
+ * Hora: 10:42 a. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+namespace ejemploBd
+{
+	partial class GestionModulos
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dgvModulos = new System.Windows.Forms.DataGridView();
+			this.btnVerPregunta = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dgvModulos)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dgvModulos
+			// 
+			this.dgvModulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvModulos.Location = new System.Drawing.Point(137, 68);
+			this.dgvModulos.Name = "dgvModulos";
+			this.dgvModulos.ReadOnly = true;
+			this.dgvModulos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+			this.dgvModulos.Size = new System.Drawing.Size(501, 189);
+			this.dgvModulos.TabIndex = 1;
+			// 
+			// btnVerPregunta
+			// 
+			this.btnVerPregunta.Location = new System.Drawing.Point(303, 365);
+			this.btnVerPregunta.Name = "btnVerPregunta";
+			this.btnVerPregunta.Size = new System.Drawing.Size(194, 50);
+			this.btnVerPregunta.TabIndex = 2;
+			this.btnVerPregunta.Text = "Ver pregunta";
+			this.btnVerPregunta.UseVisualStyleBackColor = true;
+			// 
+			// GestionModulos
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(788, 499);
+			this.Controls.Add(this.btnVerPregunta);
+			this.Controls.Add(this.dgvModulos);
+			this.Name = "GestionModulos";
+			this.Text = "GestionModulos";
+			((System.ComponentModel.ISupportInitialize)(this.dgvModulos)).EndInit();
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.Button btnVerPregunta;
+		private System.Windows.Forms.DataGridView dgvModulos;
+	}
+}
---
+++ ejemploBd/GestionModulos.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ejemploBd
+{
+    public partial class GestionModulos : Form
+    {
+        // EXAMEN PASO 1: Cadena de conexión configurada correctamente
+        private string cadenaConexion = "Server=localhost;Database=peducativa;Uid=root;Pwd=;";
+        
+        // Esta variable se usará para rastrear si es un registro nuevo (-1) o uno existente
+        private int id = -1;
+
+        public GestionModulos()
+        {
+            InitializeComponent();
+            CargarModulos();
+        }
+
+        private void CargarModulos()
+        {
+            try {
+                using (MySqlConnection conexion = new MySqlConnection(cadenaConexion)) {
+                    // EXAMEN PASO 2: Consulta bilingüe completa (Selecciona id, nombre_es y nombre_en)
+                    string consulta = "SELECT id, nombre_es, nombre_en FROM modulo";
+                    
+                    conexion.Open();
+                    MySqlDataAdapter adaptador = new MySqlDataAdapter(consulta, conexion);
+                    DataTable tabla = new DataTable();
+                    adaptador.Fill(tabla);
+                    dgvModulos.DataSource = tabla;
+                }
+            } catch (Exception ex) {
+                MessageBox.Show("Error al cargar: " + ex.Message);
+            }
+        }
+
+        void BtnVerPreguntasClick(object sender, EventArgs e)
+        {
+            // Verificamos que realmente haya una fila seleccionada para evitar que el programa se caiga
+            if (dgvModulos.SelectedRows.Count == 0) 
+            {
+                MessageBox.Show("Por favor, seleccione un módulo de la lista.");
+                return;
+            }
+
+            // EXAMEN PASO 4: Obtener el ID y Nombre del módulo seleccionado para enviarlo al formulario hijo
+            int idModulo = Convert.ToInt32(dgvModulos.SelectedRows[0].Cells["id"].Value);
+            string nombreMod = dgvModulos.SelectedRows[0].Cells["nombre_es"].Value.ToString();
+
+            // Abrir el formulario de preguntas pasando los parámetros requeridos
+            GestionPreguntas frm = new GestionPreguntas(idModulo, nombreMod);
+            frm.ShowDialog();
+        }
+    }
+}
