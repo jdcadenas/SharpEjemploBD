da27d828d683ea2a4ab4a677d4b003f39613f8f8
nabil <hhgjjjj83@gmail.com>
2026-05-15 11:26:18 -0400

    Update GestionPreguntas.cs

--- evaluacion/ejemploBd/GestionPreguntas.cs
+++ evaluacion/ejemploBd/GestionPreguntas.cs
@@ -6,60 +6,71 @@ using System.Data;
 
 namespace ejemploBd
 {
-	public partial class GestionPreguntas : Form
-	{
-		// EXAMEN PASO 5.1: Esta cadena debe estar vacía. Cópiala de MainForm.cs
-private string cadenaConexion =  "Server=localhost;Database=peducativa;Uid=root;Pwd=;";
-		private int id = -1;
-		
-		public MainForm()
-		{
-			//
-			// The InitializeComponent() call is required for Windows Forms designer support.
-			//
-			InitializeComponent();
-			CargarUsuarios();
-			
-		}
-		
-		public void CargarUsuarios()
-		{
-			
-			try {
-		
-		// EXAMEN PASO 5.2: El constructor debe recibir el ID y el Nombre del módulo
-			using (MySqlConnection conexion = new MySqlConnection(cadenaConexion))
-				{
-				
-					string consulta = "SELECT  id, nombre, clave, rol from usuario";
-					
-					conexion.Open();
-		
-					MySqlDataAdapter adaptador = new MySqlDataAdapter(consulta, conexion);
-					DataTable tabla = new DataTable(); 
-					adaptador.Fill(tabla);
-				
-					dgvUsuarios.DataSource = tabla;
-					lblEstado.Text = string.Format("Cargados {0} usuarios.", tabla.Rows.Count);
-					
-		}
+    public partial class GestionPreguntas : Form
+    {
+        // EXAMEN PASO 5.1: Esta cadena debe estar vacía. Cópiala de MainForm.cs
+        private string cadenaConexion = "Server=localhost;Database=peducativa;Uid=root;Pwd=;";
+        private int _idModulo = -1;
 
-		private void CargarPreguntas()
-		{
-			try {
-				using (MySqlConnection conexion = new MySqlConnection(cadenaConexion)) {
-					// EXAMEN PASO 6: Filtrar las preguntas por el ID del módulo recibido (_idModulo)
-					string sql = "SELECT * FROM pregunta WHERE id_modulo = " + _idModulo;
-					
-					conexion.Open();
-					MySqlDataAdapter adp = new MySqlDataAdapter(sql, conexion);
-					DataTable dt = new DataTable();
-					adp.Fill(dt);
-					dgvPreguntas.DataSource = dt;
-				}
-			} catch (Exception ex) {
-				MessageBox.Show("Error: " + ex.Message);
-			}
-		}
-	}
+        // EXAMEN PASO 5.2: El constructor debe recibir el ID y el Nombre del módulo
+        public GestionPreguntas(int idModulo, string nombreModulo)
+        {
+            //
+            // The InitializeComponent() call is required for Windows Forms designer support.
+            //
+            InitializeComponent();
+            this._idModulo = idModulo;
+            this.Text = "Preguntas de: " + nombreModulo;
+            CargarPreguntas();
+        }
+
+        public void CargarUsuarios()
+        {
+            try {
+                using (MySqlConnection conexion = new MySqlConnection(cadenaConexion))
+                {
+                    string consulta = "SELECT id, nombre, clave, rol from usuario";
+                    
+                    conexion.Open();
+
+                    MySqlDataAdapter adaptador = new MySqlDataAdapter(consulta, conexion);
+                    DataTable tabla = new DataTable(); 
+                    adaptador.Fill(tabla);
+                
+                    dgvUsuarios.DataSource = tabla;
+                    lblEstado.Text = string.Format("Cargados {0} usuarios.", tabla.Rows.Count);
+                }
+            } catch (Exception ex) {
+                MessageBox.Show(string.Format("No se pudo realizar conexion por : {0}",ex.Message));
+            }
+        }
+
+        void BtnAgregarUsuarioClick(object sender, EventArgs e)
+        {
+            UsuarioNuevo frmnuevo = new UsuarioNuevo();
+            
+            if (frmnuevo.ShowDialog() == DialogResult.OK) // para mantener estructura
+            {
+                CargarUsuarios();
+            }
+        }
+
+        private void CargarPreguntas()
+        {
+            try {
+                using (MySqlConnection conexion = new MySqlConnection(cadenaConexion)) {
+                    // EXAMEN PASO 6: Filtrar las preguntas por el ID del módulo recibido (_idModulo)
+                    string sql = "SELECT * FROM pregunta WHERE id_modulo = " + _idModulo;
+                    
+                    conexion.Open();
+                    MySqlDataAdapter adp = new MySqlDataAdapter(sql, conexion);
+                    DataTable dt = new DataTable();
+                    adp.Fill(dt);
+                    dgvPreguntas.DataSource = dt;
+                }
+            } catch (Exception ex) {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
 }
