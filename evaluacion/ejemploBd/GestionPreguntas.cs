44bafa383fc9fbcd22c37a6d204209adb5c2496c
Jane Doe <john@doe.com>
2026-05-15 15:17:21 -0400

    GestionPreguntasMujica

--- evaluacion/ejemploBd/GestionPreguntas.cs
+++ evaluacion/ejemploBd/GestionPreguntas.cs
@@ -9,17 +9,19 @@ namespace ejemploBd
 	public partial class GestionPreguntas : Form
 	{
 		// EXAMEN PASO 5.1: Esta cadena debe estar vacía. Cópiala de MainForm.cs
-		private string cadenaConexion = ""; 
+		private string cadenaConexion =  "Server=localhost;Database=peducativa;Uid=root;Pwd=;";
 		private int _idModulo;
+		private string _nombreModulo;
 
 		// EXAMEN PASO 5.2: El constructor debe recibir el ID y el Nombre del módulo
-		public GestionPreguntas( _______ _____ ,____ _____ )
+		public GestionPreguntas(int idModulo, string NombreMod)
 		{
-			InitializeComponent();
 			// El estudiante debe añadir los parámetros y asignar los valores aquí
-			
+			this._idModulo = idModulo;
+			this._nombreModulo = NombreMod;
 			//
-			//lamar a funcion 
+			//llamar a funcion 
+			CargarPreguntas();
 		}
 
 		private void CargarPreguntas()
@@ -41,3 +43,4 @@ namespace ejemploBd
 		}
 	}
 }
+
