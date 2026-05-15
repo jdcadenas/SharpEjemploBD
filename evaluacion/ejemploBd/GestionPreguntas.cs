1009a28f655ecf24fd10bf9af7f4a4212825b455
GabrielKent04 <susejvq25@gmail.com>
2026-05-15 11:04:30 -0400

    Añadiendo parametros y asignando valores en constructor de GestionPreguntas y llamando a la funcion

--- evaluacion/ejemploBd/GestionPreguntas.cs
+++ evaluacion/ejemploBd/GestionPreguntas.cs
@@ -9,17 +9,19 @@ namespace ejemploBd
 	public partial class GestionPreguntas : Form
 	{
 		// EXAMEN PASO 5.1: Esta cadena debe estar vacía. Cópiala de MainForm.cs
-		private string cadenaConexion = ""; 
+		private string cadenaConexion = "Server=localhost;Database=peducativa;Uid=root;Pwd=;"; 
 		private int _idModulo;
+		private string _NombreModulo;
 
 		// EXAMEN PASO 5.2: El constructor debe recibir el ID y el Nombre del módulo
-		public GestionPreguntas( _______ _____ ,____ _____ )
+		public GestionPreguntas(int idModulo, string modulonombre)
 		{
 			InitializeComponent();
 			// El estudiante debe añadir los parámetros y asignar los valores aquí
+			this._idModulo = idModulo;
+			this._NombreModulo = modulonombre;
 			
-			//
-			//lamar a funcion 
+			CargarPreguntas();
 		}
 
 		private void CargarPreguntas()
@@ -27,7 +29,7 @@ namespace ejemploBd
 			try {
 				using (MySqlConnection conexion = new MySqlConnection(cadenaConexion)) {
 					// EXAMEN PASO 6: Filtrar las preguntas por el ID del módulo recibido (_idModulo)
-					string sql = "SELECT * FROM pregunta WHERE id_modulo = " + _idModulo;
+					string sql = "SELECT * FROM pregunta WHERE id_modulo = _idModulo" + _idModulo;
 					
 					conexion.Open();
 					MySqlDataAdapter adp = new MySqlDataAdapter(sql, conexion);
